5d5dcacd1818046fc5c86951f77340d3cb420ad1
Anieextreme <anieextreme@gmail.com>
2022-02-23 16:55:04 +0100

    v.0.2
    Startseite Geupdatet
    Seite Animes Wurde erstellt
    Slideshow für Startseite wurde erstellt

---
+++ Startseite/Slideshow/slideshow.js
@@ -0,0 +1,31 @@
+    const indikatoren = document.getElementsByClassName("indikator");
+    indikatoren[0].classList.add("aktiv");
+
+    const slides = document.getElementsByClassName("slide")
+    slides[0].classList.add("aktiv");
+
+    var aktuellerIndex = 0;
+
+    function umschalten(anzahl) {
+        var neuerIndex = aktuellerIndex + anzahl;
+
+        if(neuerIndex < 0) {
+            neuerIndex = slides.length -1;
+        }
+
+        if(neuerIndex > slides.length -1) {
+            neuerIndex = 0;
+        }
+
+        springeZuEintrag(neuerIndex);
+    }
+
+    function springeZuEintrag(neuerIndex) {
+        indikatoren[aktuellerIndex].classList.remove("aktiv");
+        slides[aktuellerIndex].classList.remove("aktiv");
+
+        indikatoren[neuerIndex].classList.add("aktiv");
+        slides[neuerIndex].classList.add("aktiv");
+
+        aktuellerIndex = neuerIndex;
+    }
